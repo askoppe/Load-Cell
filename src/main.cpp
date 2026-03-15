a360193c80d044fec6e0865a5d9e24048a5d6de9
Alexander Skoppe <askoppe241@gmail.com>
2026-03-15 11:05:27 -0400

    Update sensor data reading and force calculation
    Refactor sensor data reading to combine bytes and calculate force.

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,15 @@ void loop()
 {
   Wire.requestFrom(SENSOR_ADDRESS, 2);    // request 2 bytes from sensor
   if (Wire.available() >= 2) {
-    byte x1 = Wire.read();
-    byte x0 = Wire.read();
-
-
-
+    byte x1 = Wire.read(); // read first byte
+    byte x2 = Wire.read(); // read second byte
+    int sensorValue = (x1 << 8) | x2; // combine bytes
+    Serial.print("Sensor Value(raw): ");
+    Serial.println(sensorValue);
+    // Convert raw value to physical units (example conversion)
+    int Force = 10*(sensorValue-1000)/15000; // LBS
+    Serial.print("Force (LBS): ");
+    Serial.println(Force);
   } else {
     Serial.println("Sensor data not available");
   }
